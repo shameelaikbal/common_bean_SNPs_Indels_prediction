ac05da40b8703aaf9b21cf2f022710749b257963
shameela ikbal <91050210+shameelaikbal@users.noreply.github.com>
2026-09-22 11:39:22 +0800

    Update 01_SNP_calling.sh

--- 01_SNP_calling.sh
+++ 01_SNP_calling.sh
@@ -41,4 +41,21 @@ time srun -m block:block:block trimmomatic PE -threads 8 \
   batch_trimmed_paired/${SAMPLE}_2_paired.fq.gz batch_trimmed_unpaired/${SAMPLE}_2_unpaired.fq.gz \
   LEADING:10 TRAILING:10 SLIDINGWINDOW:4:15 MINLEN:40
 
-#the trimming parameters i have followed Wiersma et al 2024,
+#the trimming parameters i have followed Wiersma et al 2024
+
+#next neeed to do index for bwa-mem2, samtools, and create a dict for gatk for next steps
+module load bwa-mem2/2.2.1--hd03093a_2
+bwa-mem2 index pangenome.fasta.gz #given as a job with 16 CPUs
+
+module load samtools/1.15--h3843a85_0
+samtools faidx pangenome.fasta.gz
+
+ module load gatk4/4.2.5.0--hdfd78af_0
+ gatk CreateSequenceDictionary -R pangenome.fasta.gz #given as a job
+
+ #next step is bwa-mem2 alignent to the single reference genome or pangenome fasta
+ 
+
+
+
+
